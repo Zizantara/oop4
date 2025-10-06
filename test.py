b29afe11f6ddfa8e3f59a02a18a2b1583fccc07f
Zizantara <Zizantara65@gmail.com>
2025-10-06 10:32:58 +0700

    first commit

---
+++ test.py
@@ -0,0 +1,60 @@
+# x = 5
+# y = "Zizan"
+# print(x)
+# print(y)
+
+# x = 4 # x bertipe int
+# y = "Sally" # y bertipe str
+# print(x)
+
+# x = str(3)
+# y = int(3)
+# z = float(3)
+# print(type(x))
+# print(type(y))
+# print(type(z))
+
+# x = 5
+# y = "John"
+# print(int(x))
+# print(str(y))
+
+# x = "John"
+# x = 'John'
+# print(x)
+
+# a = '4'
+# A = 'John'
+# print(A)
+
+# x, y, z = "Orange", "Banana", "Cherry"
+# print(x)
+# print(y)
+# print(z)
+
+# x = y = z = "Orange", "Banana", "Cheryy"
+# print(x)
+# print(y)
+# print(z)
+
+# fruits = ["Apple", "Banana", "Cherry"]
+# x, y, z = fruits
+# print(x)
+# print(y)
+# print(z)
+
+# x = "awesome"
+# print("Python is " + x)
+
+# x = "Python is "
+# y = "awesome"
+# z = x + y
+# print(z)
+
+# x = 5
+# y = 10
+# print( x + y)
+
+x = 5
+y = "John"
+print(int(x) * str(y))
